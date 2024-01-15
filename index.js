ee7a375c36d6d69da9986faf9012e9af549a7302
MissMoneyBags <106129216+DanielaDeMoura@users.noreply.github.com>
2024-01-15 01:27:23 +0000

    added starter files and started basic js file.

---
+++ index.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+
+    for (let hour = 9; hour <= 17; hour++) {
+      let timeblock = $("<div>").addClass("row time-block");
+      let hourDiv = $("<div>").addClass("col-md-1 hour").text(dayjs().hour(hour).format("hA"));
+      let eventInput = $("<textarea>").addClass("col-md-10 description");
+      let saveBtn = $("<button>").addClass("col-md-1 saveBtn").html("<i class='fas fa-save'></i>");
+  
+  
+      saveBtn.click(function () {
+     
+        let eventText = eventInput.val();
+        localStorage.setItem(`event-${hour}`, eventText);
+      });
+  
+      // timeblock elements
+      timeblock.append(hourDiv, eventInput, saveBtn);
+  
+      $(".container").append(timeblock);
+  
+      let savedEvent = localStorage.getItem(`event-${hour}`);
+      if (savedEvent) {
+        eventInput.val(savedEvent);
+      }
+  
+      if (hour < dayjs().hour()) {
+        timeblock.addClass("past");
+      } else if (hour === dayjs().hour()) {
+        timeblock.addClass("present");
+      } else {
+        timeblock.addClass("future");
+      }
+    }
+  });
+  
